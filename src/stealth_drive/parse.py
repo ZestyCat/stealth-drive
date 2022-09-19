5b6bde059af8053cee99ea70a37246023538f6ac
greg <g.bizup@gmail.com>
2022-09-19 15:30:03 -0700

    find images

--- src/stealth_drive/parse.py
+++ src/stealth_drive/parse.py
@@ -1,11 +1,22 @@
 import re
+from urllib.parse import urlsplit
+from bs4 import BeautifulSoup
 from validate_email import validate_email
 
-def find_emails(text):
-    emails = re.findall(r"[a-z0-9\.\-]+@[a-z0-9\.\-+_]+\.[a-z]+", text, re.I)
+def find_emails(obj, is_response=False):
+    if is_response:
+        obj = obj.text
+    emails = re.findall(r"[a-z0-9\.\-]+@[a-z0-9\.\-+_]+\.[a-z]+", obj, re.I)
     emails = [email for email in emails if validate_email(email)]
     if len(emails):
         print(f"Found {len(emails)}: {emails}")
         return emails
     else:
         return ""
+
+def find_images(obj, is_response=False):
+    if is_response:
+        obj = obj.text
+    soup = BeautifulSoup(obj)
+    img = soup.find_all("img")
+    return [i.attrs["src"] for i in img]
