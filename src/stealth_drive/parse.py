c163d418b7674ec026ec936c750221ac16b212c4
greg <g.bizup@gmail.com>
2022-09-22 08:41:01 -0700

    phone scrape

--- src/stealth_drive/parse.py
+++ src/stealth_drive/parse.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from validate_email import validate_email
 
 def find_emails(obj):
+    """ obj may be sting or Requests response object """
     if "Response" in str(type(obj)):
         obj = obj.text
     emails = re.findall(r"[a-z0-9\.\-]+@[a-z0-9\.\-+_]+\.[a-z]+", obj, re.I)
@@ -15,8 +16,20 @@ def find_emails(obj):
         return ""
 
 def find_images(obj):
+    """ obj may be sting or Requests response object """
     if "Response" in str(type(obj)):
         obj = obj.text
     soup = BeautifulSoup(obj)
     img = soup.find_all("img")
     return [i.attrs["src"] for i in img]
+
+def find_phone(obj):
+    """ obj may be sting or Requests response object """
+    if "Response" in str(type(obj)):
+        obj = obj.text
+    numbers = re.findall(r"1{0, 1}.*?[0-9]{3}.*?[0-9]{3}.*?[0-9]{4}", obj, re.I)
+    if len(numbers):
+        print(f"Found {len(numbers)}: {numbers}")
+        return numbers
+    else:
+        return ""
