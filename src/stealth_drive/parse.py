3f198ad3f3969185faa84f9db1444f4415a2f560
Greg Bizup <g.bizup@gmail.com>
2022-09-22 21:55:57 -0700

    find_contact_url

--- src/stealth_drive/parse.py
+++ src/stealth_drive/parse.py
@@ -33,14 +33,19 @@ def find_phone(obj):
         return ""
 
 def find_contact_url(obj, base_url=None):
-    """ obj may be sting or Requests response object """
-    if "Response" in str(type(obj)):
-        obj = obj.text
-    soup = BeautifulSoup(obj)
-    contact_url = str(soup.find("a", {"href" : re.compile("contact")}).get("href"))
-    if not contact_url.startswith("http") and base_url:
-        contact_url = urljoin(base_url, contact_url)
-    return contact_url
+    """ obj may be sting or Requests response object 
+        search a page for a "contact" section
+    """
+    try:
+        if "Response" in str(type(obj)):
+            obj = obj.text
+        soup = BeautifulSoup(obj)
+        contact_url = str(soup.find("a", {"href" : re.compile("contact")}).get("href"))
+        if not contact_url.startswith("http") and base_url:
+            contact_url = urljoin(base_url, contact_url)
+        return contact_url
+    except Exception as e:
+        raise e
 
 def find_phone_and_email(obj):
     """ obj may be sting or Requests response object """
