8f7d9b37dc02e7c2e66f2555b99ade306dd8c6f5
greg <g.bizup@gmail.com>
2022-09-29 11:10:20 -0700

    parse multiple replace

--- src/stealth_drive/parse.py
+++ src/stealth_drive/parse.py
@@ -77,5 +77,5 @@ def find_phone_and_email(obj):
     return phone, email
 
 def multiple_replace(text, dict):
-    regex = re.compile("(%s)" % "|".join(map(re.escape, dict.keys())))
+    regex = re.compile("(\b%s\b)" % "|".join(map(re.escape, dict.keys())))
     return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], text) 
