9ec34e7243da19197a38ed6e1ce3276f09c85b10
Greg Bizup <g.bizup@gmail.com>
2022-10-04 17:02:12 -0700

    get until got decorator

--- src/stealth_drive/selenium_functions.py
+++ src/stealth_drive/selenium_functions.py
@@ -5,14 +5,12 @@ from selenium.webdriver.common.by import By
 def fill_form(driver, text, element, by = By.ID, timeout=10):
     input = WebDriverWait(driver, timeout).until( \
         EC.presence_of_element_located((by, element)))
-    print("sending input...")
     input.clear()
     input.send_keys(text)
    
 def click_item(driver, element, by = By.ID, timeout=10):
     btn = WebDriverWait(driver, timeout).until( \
         EC.element_to_be_clickable((by, element)))
-    print("clicking button...")
     driver.execute_script("arguments[0].click();", btn)
     
 def get_element_text(driver, element, by = By.ID, timeout=10):
