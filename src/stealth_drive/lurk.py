bb2d4b71b657a5530d6fdf743224f9aa84f1bc2c
Greg Bizup <g.bizup@gmail.com>
2022-10-14 08:39:28 -0700

    follower list

--- src/stealth_drive/lurk.py
+++ src/stealth_drive/lurk.py
@@ -1,15 +1,14 @@
 from collections import deque
-from inspect import Attribute
+from requests.api import get
 from selenium.common.exceptions import TimeoutException
 import googleapiclient.discovery
 from urllib.parse import urlsplit, quote_plus, urljoin
-from fake_useragent import UserAgent
 import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
-import random
 import requests
 import re
 import os
+import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -66,73 +65,6 @@ def get_elements(driver, element, by = By.ID, timeout=10):
     ele = driver.find_elements(by, element)
     return ele
 
-class StealthDriver():
-    """ Undetected chromdriver with rotating proxies """
-    def __init__(self, proxy=None, free_proxies=False, load_images=False, **kwargs):
-        if free_proxies:
-            print("getting proxies...")
-            self.proxies = self.get_proxies(**kwargs)
-        self.init_driver(proxy=proxy, load_images=load_images)
-
-    def init_driver(self, proxy=None, load_images=False):
-        if hasattr(self, "driver"):
-            self.driver.quit()
-        ua = UserAgent()
-        userAgent = ua.random
-        options = uc.ChromeOptions()
-        if not load_images:
-            prefs = {"profile.managed_default_content_settings.images": 2}
-            options.add_experimental_option("prefs", prefs)
-        options.add_argument(f"--user-agent={userAgent}")
-        if proxy:
-            options.add_argument(f"--proxy-server={proxy}")
-        self.driver = uc.Chrome(options=options, use_subprocess=True)
-
-    def get_proxies(self, https=True, countries=None):
-        url = "https://free-proxy-list.net/"
-        html = requests.get(url).content
-        soup = BeautifulSoup(html, features="lxml")
-        rows = soup.find_all("table")[0].find_all("tr")
-        table_data = []
-        for row in rows:
-            data = [td.text for td in row]
-            if https:
-                data = [d for d in data if data[6] == "yes"]
-            if countries:
-                data = [d for d in data if \
-                        data[2] in countries or data[3] in countries]
-            if len(data) != 0:
-                table_data.append(data)
-        return [f"{row[0]}:{str(row[1])}" for row in table_data]
-
-    def get(self, url, callback=None, **kwargs):
-        if not hasattr(self, "driver"):
-            self.init_driver()
-        self.driver.get(url)
-        if callback:
-            try:
-                callback(self.driver, **kwargs)
-            except:
-                raise Exception
-
-    def proxy_get(self, url, callback=None, **kwargs):
-        while True:
-            try:
-                p = random.randint(0, len(self.proxies) - 1)
-                self.init_driver(self.proxies[p])
-                self.driver.get(url)
-                print(f"using proxy {self.proxies[p]}.")
-                if callback:
-                    try:
-                        callback(self.driver, **kwargs)
-                        break
-                    except:
-                        raise Exception
-                break
-            except:
-                print("rotating to another proxy...")
-                self.driver.quit()
-
 def get_proxies(https = True, countries = None):
     url = "https://free-proxy-list.net/"
     html = requests.get(url).content
@@ -181,7 +113,6 @@ def spb_get(api_key, url, headers=None, try_requests=True, premium=False):
                 "http": f"http://{api_key}:render_js=False@proxy.scrapingbee.com:8886",
                 "https": f"https://{api_key}:render_js=False@proxy.scrapingbee.com:8887"
             }
-
         print(proxies)
         r = requests.get(url, proxies=proxies, headers=headers, verify=False)
         return r
@@ -201,7 +132,6 @@ def crawl(start_url, proxy=False, proxies=None, callback=None, **kwargs):
     """Generator function does callback on response text"""
     new_urls = deque([start_url])
     processed_urls = set()
-
     while len(new_urls):
         url = new_urls.popleft()
         processed_urls.add(url)
@@ -216,9 +146,7 @@ def crawl(start_url, proxy=False, proxies=None, callback=None, **kwargs):
                 response = requests.get(url)
         except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
             continue
-
         soup = BeautifulSoup(response.text)
-
         for a in soup.find_all("a"):
             link = a.attrs["href"] if "href" in a.attrs else ""
             if link.startswith("/"):
@@ -228,7 +156,6 @@ def crawl(start_url, proxy=False, proxies=None, callback=None, **kwargs):
                     link in new_urls and not \
                     link in processed_urls:
                         new_urls.append(link)
-
         if callback:
             yield callback(response, **kwargs)
         else:
@@ -257,10 +184,14 @@ def get_until_got(n_tries, logfile=None): # nice decorator to try get request n 
     return decorator
 
 class InstagramProfile():
-    def __init__(self, driver, username, posts=10):
+    def __init__(self, driver, username, posts=10, get_follower_list=True, my_username=None, my_password=None):
         self.driver = driver
+        self.username = username
         self.posts = self.get_posts(username, n=posts)
         self.data = self.get_basic_info(username)
+        if get_follower_list:
+            self.follower_list = self.get_follower_list(my_username, my_password)
+            self.data["follower_list"] = self.follower_list
         self.data["posts"] = self.posts
 
     def get_basic_info(self, username):
@@ -359,6 +290,28 @@ class InstagramProfile():
                     pass
             posts.append(post)
         return posts
+
+    def get_follower_list(self, my_username, my_password):
+        #instagram_login(my_username, my_password)
+        input("Login to instagram")
+        self.driver.get(f"https://www.instagram.com/{self.username}/followers/")
+        scroll = """
+            scrollingDiv = document.querySelector("div._aano");
+            scrollingDiv.scrollTop = scrollTopMax;
+        """
+        followers = set()
+        n = get_elements(self.driver, "span[class='_ac2a']", By.CSS_SELECTOR)[0].text
+        self.data["n_followers"] = n # Update self.data
+        while len(followers < n):
+            print(f"got {len(followers)} followers from {self.username}")
+            self.driver.execute_script(scroll)
+            time.sleep(3)
+            follower_div = get_element(self.driver, "div._aano", By.CSS_SELECTOR)
+            soup = BeautifulSoup(follower_div)
+            follower_names = [div.text for div in soup.select("div[class=' _ab8y  _ab94 _ab97 _ab9f _ab9k _ab9p _abcm']")]
+            followers.update(follower_names)
+        return list(followers)
+
 
 class InstagramInfluencer(InstagramProfile):
     def __init__(self, driver, username, session_id, apify_api_key, posts=10, follower_count=100):
